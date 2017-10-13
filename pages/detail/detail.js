954cb5593d38ee43c480a3c214f32a0d344c3aa1
jiuyan <jiuyan@jiuyandeMacBook-Pro.local>
2017-10-13 19:01:42 +0800

    finish html

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -17,7 +17,28 @@ Page({
     interval: 5000,
     duration: 1000,
     circular: true,
-    curSwiperIdx: 0
+    curSwiperIdx: 0,
+    headLine: {
+      title: "摄影外拍：秋日童话的邂逅",
+      desc: "发起人：犀牛"
+    },
+    infos: {
+      sAddr: "杭州 西湖",
+      time: "10月5日 14:00-19:00",
+      detailAddr: "杭州市西湖区万塘路8号黄龙时代广场A1802",
+      intro: "金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香……你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会…  展开全部"
+    },
+    siginInUsers: [{
+      avatar: "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg",
+      sex: "male",
+      nick: "犀牛",
+      desc: "23岁  杭州.城西  互联网产品经理"
+    }, {
+      avatar: "http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg",
+      sex: "female",
+      nick: "犀牛",
+      desc: "23岁  杭州.城西  互联网产品经理"
+    }]
   },
   onLoad: function () {
     wx.setNavigationBarTitle({
@@ -25,7 +46,6 @@ Page({
     })
   },
   swiperChange: function (e) {
-    console.log(e.detail.current)
     this.setData({
       curSwiperIdx: e.detail.current
     })
