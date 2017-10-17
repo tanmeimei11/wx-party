1cb4d10d7f0ba78a6497810c15886e8de41c0c68
maifei <maifei@in66.com>
2017-10-17 14:25:29 +0800

    wx-party

--- mock/qunList.js
+++ mock/qunList.js
@@ -1,16 +1,28 @@
 let Mock = require('../utils/mock.js')
 module.exports = Mock.mock({
   'succ': true,
-  'data|10': [{
-    'id|+1': 1,
-    'city_social_id': "11212",
-    'city_social_name': '杭州城西户外群222222222222222222222222222',
-    'female_count': 10,
-    'male_count': 11,
-    'avatar': 'http://inimg05.jiuyan.info/in/2017/07/08/1FA80AEF-1F57-27FD-FB72-D66207D45228-1xLEKMvL.jpg?imageMogr2/format/jpg/thumbnail/180x%3E/quality/80!',
-    'tags': ['90后', '活动丰富', '90后', '活动丰富', '90后', '活动丰富', '活动丰富活动丰富活动丰富活动丰富活动丰富活动丰富活动丰富', '90后', '活动丰富', '90后'],
-    'assistant_qrcode_url': 'http://inimg05.jiuyan.info/in/2017/07/08/1FA80AEF-1F57-27FD-FB72-D66207D45228-1xLEKMvL.jpg?imageMogr2/format/jpg/thumbnail/180x%3E/quality/80!',
-    'current_cursor': 32123123
-  }],
-  'time': 1502261173
+  'data|10': {
+    'act_num': 12,
+    'list|10': [{
+      'id|+1': 1,
+      'city_social_id|+1': 11212,
+      'city_social_name': '杭州城西户外群222222222222222222222222222',
+      'female_count': 10,
+      'male_count': 11,
+      'avatar': 'http://inimg05.jiuyan.info/in/2017/07/08/1FA80AEF-1F57-27FD-FB72-D66207D45228-1xLEKMvL.jpg?imageMogr2/format/jpg/thumbnail/180x%3E/quality/80!',
+      'tags': [
+        '90后', '活动丰富', '90后', '活动丰富', '90后', '活动丰富', '活动丰富活动丰富活动丰富活动丰富活动丰富活动丰富活动丰富', '90后', '活动丰富', '90后'
+      ],
+      "assistants": [
+        {
+          "assistant_url": "http://inimg05.jiuyan.info/in/2017/07/08/1FA80AEF-1F57-27FD-FB72-D66207D45228-1xLEKMvL.jpg?imageMogr2/format/jpg/thumbnail/180x%3E/quality/80!"
+        },
+        {
+          "assistant_url": "http://inimg05.jiuyan.info/in/2017/07/08/1FA80AEF-1F57-27FD-FB72-D66207D45228-1xLEKMvL.jpg?imageMogr2/format/jpg/thumbnail/180x%3E/quality/80!"
+        }
+      ],
+      'current_cursor': 32123123
+    }]
+  },
+  'timestamp': 1502261173
 })
