032a4ba7c4e5e4d4f2c31f3ac9d599d01743e7a4
xiangxiang <xiangxiang@in66.com>
2017-10-19 18:22:52 +0800

    update 10.19

--- mock/detail.js
+++ mock/detail.js
@@ -1,50 +1,52 @@
 module.exports = {
-  "msg": "",
-  "code": "200003",
-  "succ": true,
-  "data": {
-    "act_id": "10701",
-    "act_name": "龙井户外徒步",
-    "act_url": ["https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
-      'https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg',
-      'https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg'
-    ],
-    "act_location": "杭州市西湖区万塘路8号黄龙时代广场A1802",
-    "act_desc": "金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会",
-    "act_status": "0", //0：未开始 //1：已结束
-    "assistants": [{
-        "assistant_name": "xxx1",
-        "assistant_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg"
-      },
-      {
-        "assistant_name": "xxx2",
-        "assistant_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg"
-      }
-    ],
-    "city_district": "杭州 城西",
-    "creator_name": "犀牛",
-    "start_time": 1507651200000,
-    "end_time": 1507737600000,
-    "join_status": "1", //0:未参与 1:已参与  2:已签到
-    "joiners": [{
-        "age": "11",
-        "avatar_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
-        "city": "杭州",
-        "district": "西湖区",
-        "gender": "1",
-        "work": "程序员"
-      },
-      {
-        "age": "11",
-        "avatar_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
-        "city": "浙江",
-        "district": "城西",
-        "gender": "1",
-        "work": "程序员"
-      }
-    ],
-    "other_act_count": "61",
-    "act_qrcode_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
-  },
-  "timestamp": "1508137733"
+  data: {
+    "msg": "",
+    "code": "200003",
+    "succ": true,
+    "data": {
+      "act_id": "10701",
+      "act_name": "龙井户外徒步",
+      "act_url": ["https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
+        'https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg',
+        'https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg'
+      ],
+      "act_location": "杭州市西湖区万塘路8号\n黄龙时代广场A1802",
+      "act_desc": "金秋十月，\n丹桂飘香，杭城大\n街小巷，飘着淡淡的桂花香\n你说紫金港的桂花是什么味\n\n[金额]：19元\n\n道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会金秋十月，丹桂飘香，杭城大街小巷，飘着淡淡的桂花香你说紫金港的桂花是什么味道呢？秋日的桂花雨，是触手可及的浪漫，每一朵都是惊喜。让我们来一场秋日的摄影聚会",
+      "act_status": "0", //0：未开始 //1：已结束
+      "assistants": [{
+          "assistant_name": "xxx1",
+          "assistant_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg"
+        },
+        {
+          "assistant_name": "xxx2",
+          "assistant_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg"
+        }
+      ],
+      "city_district": "杭州 城西",
+      "creator_name": "犀牛",
+      "start_time": 1507651200000,
+      "end_time": 1507737600000,
+      "join_status": "1", //0:未参与 1:已参与  2:已签到
+      "joiners": [{
+          "age": "11",
+          "avatar_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
+          "city": "杭州",
+          "district": "西湖区",
+          "gender": "1",
+          "work": "程序员"
+        },
+        {
+          "age": "11",
+          "avatar_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
+          "city": "浙江",
+          "district": "城西",
+          "gender": "1",
+          "work": "程序员"
+        }
+      ],
+      "other_act_count": "61",
+      "act_qrcode_url": "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
+    },
+    "timestamp": "1508137733"
+  }
 }
