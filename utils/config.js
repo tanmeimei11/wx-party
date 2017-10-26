adbd715670cce57963c910574d01acb970441886
xiangxiang <xiangxiang@in66.com>
2017-10-26 10:40:43 +0800

    https

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   _v: '1.0.0',
   // DOMAIN: 'http://qaactivity.in66.com',
-  DOMAIN: 'http://activity.in66.com',
+  DOMAIN: 'https://activity.in66.com',
   // isMock: true
   isMock: false
 }
