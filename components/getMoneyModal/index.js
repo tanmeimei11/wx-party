136edd9e81a5e1b5287574bc2407b8b91d8a8f6a
xiangxiang <xiangxiang@in66.com>
2017-11-05 20:23:55 +0800

    add

--- components/getMoneyModal/index.js
+++ components/getMoneyModal/index.js
@@ -3,8 +3,6 @@ import track from '../../utils/track.js'
 module.exports = {
   data: {
     isShowGetMoneyModal: false,
-    avatarUrl: 'https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg',
-    actQrImg: 'https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg'
   },
   closeGetMoneyModal: function () {
     this.setData({
