db8eb232a4bf13e5b4b13a733b7c5a60ce8aa6d8
xiangxiang <xiangxiang@in66.com>
2017-11-13 21:12:58 +0800

    add

--- app.js
+++ app.js
@@ -2,11 +2,26 @@
 // var wxPromisify = require('./utils/wxPromise.js').wxPromisify
 // var requestPromisify = require('./utils/wxPromise.js').requestPromisify
 // let util = require('./utils/util.js')
+var ga = require('./lib/ga.js');
+var _v = require('./utils/config.js')._v;
+var GoogleAnalytics = ga.GoogleAnalytics;
 App({
+  // ...
+  tracker: null,
+  getTracker: function () {
+    if (!this.tracker) {
+      // 初始化GoogleAnalytics Tracker
+      this.tracker = GoogleAnalytics.getInstance(this)
+        .setAppName('in同城趴')
+        .setAppVersion(_v)
+        .newTracker('UA-109527059-1'); //用你的 Tracking ID 代替
+    }
+    return this.tracker;
+  },
   globalData: {
     code: null,
     userInfo: null
   },
-  onLaunch: function () {
-  }
+  onLaunch: function () {}
+  // ...
 })
--- utils/track.js
+++ utils/track.js
@@ -1,20 +1,57 @@
 // import common from './common'
 const U_TRACK = 'http://stats1.jiuyan.info/onepiece/router.html'
 // const U_TRACK = 'http://10.10.109.253:8018/index.html'
+// 这里是ga统计
+var ga = require('../lib/ga.js');
+
+var HitBuilders = ga.HitBuilders;
+var t = getApp().getTracker();
+t.setScreenName('测试页面');
+// t.send();
 var trackArray = []
+var gaTrackArray = []
 var isTrack = false
+
+var gaTrack = (app, track) => {
+  var _action = track.action
+  var _query = track.query
+  if (!_query) {
+    _query = 'click'
+  }
+  var entry = /.*_page_enter/.exec(_query)
+  if (entry) {
+    t.setScreenName(entry);
+    var screenName = new HitBuilders.ScreenViewBuilder().build()
+    t.send(screenName);
+  }
+  var a = new HitBuilders.EventBuilder()
+    .setCategory(_action)
+    .setAction(_query)
+  // .setLabel('1111') // 可选
+  // .setValue('34567')
+}
+
+t.send(a);
+
+}
 
 var requestTrack = (app) => {
   console.log(isTrack)
 
+  var t = getApp().getTracker();
+  t.setScreenName('这里是日志页面');
+  t.send(new HitBuilders.ScreenViewBuilder().build());
   return new Promise((resolve, reject) => {
     if (trackArray.length) {
       setTimeout(() => {
         console.log('real', trackArray[0])
+        gaTrack(app, gaTrackArray[0])
+        console.log(gaTrackArray[0])
         app.setData({
           trackSeed: trackArray[0]
         })
         trackArray.shift()
+        gaTrackArray.shift()
         return requestTrack(app)
       }, 100)
     } else {
@@ -27,6 +64,10 @@ var requestTrack = (app) => {
 export default function track(app, seed, query = []) {
   // let img = new Image()
   // img.src = combineQuery(seed, query)
+  gaTrackArray.push({
+    action: seed,
+    query: query.join('&')
+  })
   let trackSeed = combineQuery(seed, query)
   console.log('visual', trackSeed)
   // 这样请求会被丢弃 有的埋点抓不到 原因1，img赋值太快了造成页面请求还没有发就被丢弃了 2，替换的过快 setdata直接跳过类似vue 好像setdata内部也是和vue内部一样的 超过一定数量采取更新dom 
