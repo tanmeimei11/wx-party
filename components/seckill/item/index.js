3edfba81b7909c47d24583207c85329793e21c59
xiangxiang <xiangxiang@in66.com>
2017-11-22 00:44:05 +0800

    add

--- components/seckill/item/index.js
+++ components/seckill/item/index.js
@@ -5,20 +5,20 @@ module.exports = {
   initSeckill: function () {
     this.setData({
       promoList: this.data.promoList.map(item => {
-        item.time = +item.count_down / 1000
+        item.cutTime = +item.count_down / 1000
         return item
       })
     })
     this.countdown()
   },
   countdown: function () {
-    if (this.data.promoList.filter(item => item.time > 0).length == 0) {
+    if (this.data.promoList.filter(item => item.cutTime > 0).length == 0) {
       return
     }
     this.setData({
       promoList: this.data.promoList.map(item => ({
         ...item,
-        time: (item.time - 1 <= 0) ? 0 : (item.time - 1)
+        cutTime: (item.cutTime - 1 <= 0) ? 0 : (item.cutTime - 1)
       }))
     })
     setTimeout(() => this.countdown(), 1000)
