a48476e6b11ccb95d7eb27e8d53c65469840ebf9
xiaofu <xiaofu@in66.com>
2017-11-28 11:21:05 +0800

    add

--- mock/union.js
+++ mock/union.js
@@ -1,15 +1,17 @@
 module.exports = {
-  "msg": "",
-  "code": "200003",
-  "succ": true,
-  "data": {
-    "is_launch": true, //是否是发起拼团活动
-    "union_launch_avatar":"http://img05.in66.com/xxxxx.jpg", //发起人头像
-    "union_launch_nick_name":"犀牛",//发起人昵称
-    "union_join_avatar":"http://img05.in66.com/xxxxx.jpg", //拼图参与人头像
-    "union_join_nick_name":"xxx", //拼图参与人昵称
-    "count_down":"200", //倒计时 毫秒
-    "share_key":"skks",//分享的shareKey,可能为空，只有是发起人的情况才有这个字段
-  },
-  "timestamp": "1508137132"
+  data: {
+    "msg": "",
+    "code": "200003",
+    "succ": true,
+    "data": {
+      "is_launch": true, //是否是发起拼团活动
+      "union_launch_avatar":"http://img05.in66.com/xxxxx.jpg", //发起人头像
+      "union_launch_nick_name":"犀牛",//发起人昵称
+      "union_join_avatar":"http://img05.in66.com/xxxxx.jpg", //拼图参与人头像
+      "union_join_nick_name":"xxx", //拼图参与人昵称
+      "count_down":"200", //倒计时 毫秒
+      "share_key":"skks",//分享的shareKey,可能为空，只有是发起人的情况才有这个字段
+    },
+    "timestamp": "1508137132"
+}
 }
---
+++ pages/result_union/result_union.js
@@ -0,0 +1,47 @@
+import track from '../../utils/track.js'
+var request = require('../../utils/wxPromise.js').requestPromisify
+import {
+  mutulPage
+} from '../../utils/util.js'
+var seckillResult = require('../../components/seckill/result/index.js')
+mutulPage({
+  mixins: [seckillResult],
+  data: {
+    item: 0,
+
+  },
+  onLoad: function (option) {
+    track(this, 'h5_tcpa_result_screen_enter')
+    // 取页面上的id
+    this.setData({
+      id: option.id,
+      promoText: `本周在你附近举办的${option.promonum == 0 ? "" : `${option.promonum}个`}活动`,
+      sessionFrom: `activityassistant_${option.id}`,
+      transferImageUrl: option.transferImageUrl,
+      title: option.title,
+    })
+    this.setSeckillOptions(option)
+    request({
+      url: `/union/share_info`,
+      data: {
+        act_id: '',
+        launch_user_id: '',
+        _v: ''
+      }
+    }).then((res) => {
+      // console.log(res.data.map(item => {
+      //   item.cutTime = +item.count_down / 1000
+      //   return item
+      // }))
+      // this.setData({
+      //   item: res.data
+      // })
+    })
+  },
+  goBack: function () {
+    track(this, 'h5_tcpa_paysucc_look')
+    wx.redirectTo({
+      url: '../index/index?tab=2'
+    })
+  }
+})
