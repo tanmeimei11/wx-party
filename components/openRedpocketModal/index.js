2319e68db23a70a1d8536d348aa86e1f0c5b11bd
xiangxiang <xiangxiang@in66.com>
2017-12-04 17:26:57 +0800

    add

---
+++ components/openRedpocketModal/index.js
@@ -0,0 +1,26 @@
+// var request = require('../../utils/wxPromise.js').requestPromisify
+import track from '../../utils/track.js'
+module.exports = {
+  data: {
+    isShowGetMoneyModal: false
+  },
+  closeGetMoneyModal: function () {
+    this.setData({
+      isShowGetMoneyModal: false
+    })
+  },
+  lookBalance: function () {
+    track(this, 'h5_tcpa_gold_see_click')
+    wx.navigateTo({
+      url: `../balance/balance`
+    })
+    this.closeGetMoneyModal()
+  },
+  lookBalanceShare: function () {
+    track(this, 'h5_tcpa_gold_forward_click')
+    wx.navigateTo({
+      url: `../balance/balance`
+    })
+    this.closeGetMoneyModal()
+  }
+}
