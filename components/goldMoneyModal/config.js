0b04b1e7e090716718f77f0fcd7e1145f04de5f0
xiangxiang <xiangxiang@in66.com>
2017-12-05 14:02:54 +0800

    add

---
+++ components/goldMoneyModal/config.js
@@ -0,0 +1,37 @@
+var images = {
+  body: {
+    src: "https://inimg07.jiuyan.info/in/2017/11/07/D2BBB9D0-B9BD-A49A-85F5-FF91D3D5BF45.jpg",
+    local: "",
+    x: 0,
+    y: 0,
+    w: 750,
+    h: 866
+  },
+  qr: {
+    src: "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
+    local: "",
+    x: 275,
+    y: 433,
+    w: 200,
+    h: 200,
+  },
+  qrContain: {
+    src: "https://inimg07.jiuyan.info/in/2017/11/06/6934FAC6-A317-EAE6-75A5-9472183A4B91.jpg",
+    local: "",
+    x: 265,
+    y: 423,
+    w: 220,
+    h: 220,
+  },
+  avatar: {
+    src: "https://inimg01.jiuyan.info/in/2017/02/28/85929FBE-BB9D-91D5-7BA3-068EE42A6000-1JyqzdYV.jpg",
+    local: "",
+    x: 311,
+    y: 108,
+    w: 128,
+    h: 128,
+  },
+}
+module.exports = {
+  images
+}
