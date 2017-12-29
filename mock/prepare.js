bf3ae8520388518f4b92af2df4db54ce66f90f2a
xiangxiang <xiangxiang@in66.com>
2017-12-29 10:36:54 +0800

    hide btn

--- mock/prepare.js
+++ mock/prepare.js
@@ -1,45 +1,12 @@
 module.exports = {
-  data: {
-    "msg": "",
-    "code": "200003",
-    "succ": true,
-    "data": {
-      "act_qrcode_url":"https://inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg", //二维码图片地址
-      "list":[
-          {
-              "name":"犀牛",
-              "avatar_url": "https://inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg",
-              "userId": "123",
-              "phone": "18506830163",
-              "gender": "男",
-              "join_status":"2", //1 表示已报名 2 表示已签到
-          },
-          {
-              "name":"犀牛",
-              "avatar_url": "https://inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg",
-              "userId": "123",
-              "phone": "120",
-              "gender": "男",
-              "join_status":"1", //1 表示已报名 2 表示已签到
-          },
-          {
-              "name":"犀牛",
-              "avatar_url": "https://inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg",
-              "userId": "123",
-              "phone": "120",
-              "gender": "男",
-              "join_status":"1", //1 表示已报名 2 表示已签到
-          },
-          {
-              "name":"犀牛",
-              "avatar_url": "https://inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg",
-              "userId": "123",
-              "phone": "120",
-              "gender": "男",
-              "join_status":"2", //1 表示已报名 2 表示已签到
-          }
-      ]
-    },
-    "timestamp": "1508137696"
-  }
+    data: {
+        "msg": "",
+        "code": "200003",
+        "succ": true,
+        "data": {
+            "act_qrcode_url": "https://inimg01.jiuyan.info/in/2015/12/06/50E0D236-8186-87D4-278E-184399C6B81D-1ndoVWb.jpg", //二维码图片地址
+            "list": []
+        },
+        "timestamp": "1508137696"
+    }
 }
